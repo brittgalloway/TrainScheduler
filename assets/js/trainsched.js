fc903a5b53383ef0534a6d9a30675df2df7f975d
BrittGall <crlnfllr@gmail.com>
2019-09-07 01:08:12 -0500

    linking firebase

--- assets/js/trainsched.js
+++ assets/js/trainsched.js
@@ -1,3 +1,17 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAM9lhr8g_68x5GhZqS8GnGF-JDwSwgpYc",
+  authDomain: "trainschedulerweek7.firebaseapp.com",
+  databaseURL: "https://trainschedulerweek7.firebaseio.com",
+  projectId: "trainschedulerweek7",
+  storageBucket: "",
+  messagingSenderId: "683065864830",
+  appId: "1:683065864830:web:85e8455ec49dff97dc1e2f"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const database = firebase.database();
+
 $(document).ready(function() {
   //program
 });
